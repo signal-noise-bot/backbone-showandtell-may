abb8cddd7579064eecee76e252a82c921b9eef9f
Anthony Cossins <anthonyecossins@gmail.com>
2015-05-19 19:48:57 +0100

    Added gulp file and stuff

---
+++ gulp/tasks/sass.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+  gulp.src('./presentation/{,**/*}*.{scss,sass}')
+	.pipe(sourcemaps.init())
+	.pipe(sass({
+	 	errLogToConsole: true,
+	 	// outputStyle: 'compressed'
+	}))
+	.pipe(prefix("last 3 version", "> 1%", "ie 8"))
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('./presentation/css'));
+})
+
+gulp.task('default', ['sass'], function () {
+	gulp.watch('./presentation/{,**/*}*.{scss,sass}', ['sass'])
+});
