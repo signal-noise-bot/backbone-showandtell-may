c4da7b8f6bd89d601fac5beb6d1dc6bd8092091e
Chris Brown <chris@chrisbrown.io>
2015-05-19 19:47:54 +0100

    Updaed to be PHP

---
+++ presentation/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Shower Presentation Engine</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=792, user-scalable=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<link rel="stylesheet" href="highlight/styles/default.css">
+	<link rel="stylesheet" href="shower/themes/dark/styles/screen.css">
+</head>
+<body class="list">
+
+	<section class="slide"><div><?php include('slides/intro/intro-0.html'); ?></div></section>
+
+	<div class="progress"><div></div></div>
+
+	<script src="shower/shower.min.js"></script>
+
+	<script src="highlight/highlight.pack.js"></script>
+	
+</body>
+</html>
