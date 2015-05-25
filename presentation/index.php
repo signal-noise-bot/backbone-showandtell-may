2e74e7693545c802564a23564a5f9eb4a67e1046
Chris Brown <chris@chrisbrown.io>
2015-05-25 19:05:43 +0100

    Added stackedbar example slides

--- presentation/index.php
+++ presentation/index.php
@@ -12,16 +12,26 @@
 <body class="list">
 
 	<section class="slide"><div><?php include('slides/intro/intro-0.html'); ?></div></section>
+
 	<section class="slide bbpa-intro"><div><?php include('slides/bbpa/bbpa-intro.html'); ?></div></section>
-	<section class="slide bbpa-sidebar"><div><?php include('slides/bbpa/bbpa-sidebar.html'); ?></div></section>
-	<section class="slide bbpa-stacked-bars"><div><?php include('slides/bbpa/bbpa-stacked-bars.html'); ?></div></section>
+	<section class="slide bbpa-modularity"><div><?php include('slides/bbpa/bbpa-modularity.html'); ?></div></section>
+	<section class="slide bbpa-data-format"><div><?php include('slides/bbpa/bbpa-data-format.html'); ?></div></section>
+	<section class="slide bbpa-stacked-bars-markup"><div><?php include('slides/bbpa/bbpa-stacked-bars-markup.html'); ?></div></section>
+	<section class="slide bbpa-stacked-bars-example"><div><?php include('slides/bbpa/bbpa-stacked-bars-example.html'); ?></div></section>
+	<section class="slide bbpa-stacked-bars-customisability"><div><?php include('slides/bbpa/bbpa-stacked-bars-customisability.html'); ?></div></section>
+	<section class="slide bbpa-donut-intro"><div><?php include('slides/bbpa/bbpa-donut-intro.html'); ?></div></section>
+	<section class="slide bbpa-donut-markup"><div><?php include('slides/bbpa/bbpa-donut-markup.html'); ?></div></section>
+	<section class="slide bbpa-donut-customisability"><div><?php include('slides/bbpa/bbpa-donut-customisability.html'); ?></div></section>
+	<section class="slide bbpa-donut-svg-syntax"><div><?php include('slides/bbpa/bbpa-donut-svg-syntax.html'); ?></div></section>
+	<section class="slide bbpa-helper"><div><?php include('slides/bbpa/bbpa-helper.html'); ?></div></section>
+	<section class="slide bbpa-helper-example"><div><?php include('slides/bbpa/bbpa-helper-example.html'); ?></div></section>
 
 	<div class="progress"><div></div></div>
 
 	<script src="shower/shower.min.js"></script>
   <script data-main="init" src="lib/requirejs/require.js"></script>
 
-	<!-- // <script src="highlight/highlight.pack.js"></script> -->
+	<script src="highlight/highlight.pack.js"></script> -->
 	
 </body>
 </html>
